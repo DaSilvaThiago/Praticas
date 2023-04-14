bad5890618a6f65a44ffce0e5d44690bca10f2df
Thiago <silvathiago011@hotmail.com>
2023-04-14 20:14:14 -0300

    atualizando

--- exFormEmPhp/form.php
+++ exFormEmPhp/form.php
@@ -5,84 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
-<?php
-function enviar(){
-    $cadastrados = [
-        "nome"=>$nome = $_POST["nome"],
-        "apelido"=>$apelido = $_POST["apelido"],
-        "foto"=> $foto = $_POST["foto"],
-        "data" =>$data = $_POST["data"],
-        "bio"=>$bio = $_POST["bio"],
-        "genero"=>$genero = $_POST["genero"]];      
-
-        if(isset($nome)){
-            echo "digite seu nome!";
-        }
-        if(isset($apelido)){
-            echo "digite seu apelido!";
-        }
-        if(isset($foto)){
-            echo "escolha uma foto!";
-        }
-        if(isset($data)){
-            echo "digit sua data de nascimento!";
-        }
-        if(isset($bio)){
-            echo "escreve um texto breve!";
-        }
-        if(isset($genero)){
-           echo "escolha um genêro!";
-        }
-    }
-     /*   class cadastro{
-           private $nome;
-           private $apelido;
-           private $foto;
-           private $data;
-           private $bio;
-           private $genero;
-   
-           public function __construct($nome, $apelido, $foto, $data, $bio, $genero){
-               $this -> nome = $nome;
-               $this -> apelido = $apelido;
-               $this -> foto = $foto;
-               $this -> data = $data;
-               $this -> bio = $bio;
-               $this -> genero = $genero;
-           }
-   
-           public function escrever(){
-               return $this->nome;
-           }
-       }
-   */
-?>
-
 </head>
 <body>
     <h1 style="text-align:center">Formulário</h1>
     <fieldset style="width:200px; margin:auto">
-        <form action="" method="POST">
+        <form action="resultadoForm.php" method="POST">
+            <?php
+                $cadastrados = [
+                    "nome"=>$nome = $_POST["nome"],
+                    "apelido"=>$apelido = $_POST["apelido"],
+                    "foto"=> $foto = $_POST["foto"],
+                    "data" =>$data = $_POST["data"],
+                    "bio"=>$bio = $_POST["bio"],
+                    "genero"=>$genero = $_POST["genero"]];
+            ?>
             <p>
                 <label for="nome">Nome: </label>
-                <input type="text" name="nome" id="nome">
+                <input type="text" name="nome" id="nome" maxlength="21" require>
             </p>
             <p>
                 <label for="apelido">Apelido: </label>
-                <input type="text" id="apelido" name="apelido"> 
+                <input type="text" id="apelido" name="apelido" maxlength="21" require> 
             </p>
             <p>
                 <label for="foto">Foto: </label>
-                <input type="file" id="foto" name="foto">                
+                <input type="file" id="foto" name="foto" require>                
             </p>
             <p>
                 <label for="dataNas">Data de Nascimento: </label>
-                <input type="date" id="data" name="data">               
+                <input type="date" id="data" name="data" require>               
             </p>
             <p>
                 <label for="bio"></label>
-                <textarea name="bio" id="bio" cols="30" rows="10"></textarea> 
+                <textarea name="bio" id="bio" cols="30" rows="10" require></textarea> 
             </p>
             <p>
                 <label for="genero">Gênero: </label>
@@ -92,7 +47,7 @@ function enviar(){
                 <input type="radio" name="genero" id="feminino" value="f">
             </p>
             <p>
-                <button type="submit" onclick="enviar()">
+                <button type="submit">
                     Enviar
                 </button>
             </p>
