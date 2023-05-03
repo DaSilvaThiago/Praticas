06d4c9e94e2944e8e0eb0daefa374047d781b8ea
Thiago Dias da Silva <silvathiago011@hotmail.com>
2023-05-03 00:53:38 -0300

    att

--- treinoCrud/jeitoVictor/finalizaConexao.php
+++
@@ -1,4 +0,0 @@
-<?php
-    if($transacaoOK) $pdo -> commit(); else $pdo-> rollback();
-    $pdo = null;
-?>
--- treinoCrud/jeitoVictor/iniciaConexao.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include_once "conexao.php"; 
-    $pdo = conexao();
-    $transacaoOK = false;
-    $pdo -> beginTransaction(); 
-?>
